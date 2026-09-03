870edc1bddc7f4cbbd66c0536c7c70804a0b81ee
thejunkheroes <thejunkheroesllc@gmail.com>
2026-09-03 03:11:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE whenever you edit index.html, then reload twice on each phone. */
-const CACHE = 'jh-clock-v21';
+const CACHE = 'jh-clock-v22';
 const FILES = [
   './', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'
